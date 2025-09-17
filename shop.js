930783c4da82ad4b24a21e0f679e4dc54580038e
yvettejdevries-byte <yvette.j.de.vries@gmail.com>
2025-09-17 19:49:08 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -6,12 +6,16 @@ const PRODUCTS = {
 
 function getBasket() {
   const basket = localStorage.getItem("basket");
-  return basket ? JSON.parse(basket) : [];
+  return basket ? JSON.parse(basket) : {};
 }
 
 function addToBasket(product) {
   const basket = getBasket();
-  basket.push(product);
+  if (basket[product]) {
+    basket[product] += 1;
+  } else {
+    basket[product] = 1;
+  }
   localStorage.setItem("basket", JSON.stringify(basket));
 }
 
@@ -25,16 +29,20 @@ function renderBasket() {
   const cartButtonsRow = document.querySelector(".cart-buttons-row");
   if (!basketList) return;
   basketList.innerHTML = "";
-  if (basket.length === 0) {
+
+  const basketKeys = Object.keys(basket);
+  if (basketKeys.length === 0) {
     basketList.innerHTML = "<li>No products in basket.</li>";
     if (cartButtonsRow) cartButtonsRow.style.display = "none";
     return;
   }
-  basket.forEach((product) => {
-    const item = PRODUCTS[product];
+
+  basketKeys.forEach((productKey) => {
+    const quantity = basket[productKey];
+    const item = PRODUCTS[productKey];
     if (item) {
       const li = document.createElement("li");
-      li.innerHTML = `<span class='basket-emoji'>${item.emoji}</span> <span>${item.name}</span>`;
+      li.innerHTML = `<span class='basket-emoji'>${item.emoji}</span> <span>${quantity}x ${item.name}</span>`;
       basketList.appendChild(li);
     }
   });
@@ -51,8 +59,10 @@ function renderBasketIndicator() {
     indicator.className = "basket-indicator";
     basketLink.appendChild(indicator);
   }
-  if (basket.length > 0) {
-    indicator.textContent = basket.length;
+  const basketKeys = Object.keys(basket);
+  if (basketKeys.length > 0) {
+    const totalQuantity = basketKeys.reduce((sum, key) => sum + basket[key], 0);
+    indicator.textContent = totalQuantity;
     indicator.style.display = "flex";
   } else {
     indicator.style.display = "none";
